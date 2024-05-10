3c25cb58f16de50b90126ab3b280de5e74389467
Elie BINGUE <eliebingue7@icloud.com>
2024-05-10 23:58:31 +0200

    Mise en place du fichier de données (menu), création de la liste de menu

---
+++ MenuMiam/MenuItem.swift
@@ -0,0 +1,16 @@
+//
+//  MenuItem.swift
+//  MenuMiam
+//
+//  Created by Elie BINGUE on 10/05/2024.
+//
+
+import Foundation
+
+struct MenuItem: Identifiable {
+	 
+	 var id: UUID = UUID()
+	 var name: String
+	 var price: String
+	 var imageName: String
+}
