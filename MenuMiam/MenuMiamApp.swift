3c200ed73f690c9e4a5ab9cb1c13cd50b0081524
Elie BINGUE <eliebingue7@icloud.com>
2024-05-10 14:09:40 +0200

    Initial Commit

---
+++ MenuMiam/MenuMiamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuMiamApp.swift
+//  MenuMiam
+//
+//  Created by Elie BINGUE on 10/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MenuMiamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
